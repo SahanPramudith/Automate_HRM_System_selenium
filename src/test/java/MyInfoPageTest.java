1abbd80d872b0ca65aba44852c1b20fc430836c3
sahanpramudith <sahanpramudith466@gmail.com>
2025-08-29 15:00:04 +0530

    Add MyInfo page and corresponding test for navigation and URL verification

---
+++ src/test/java/MyInfoPageTest.java
@@ -0,0 +1,17 @@
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.MyInfo;
+
+public class MyInfoPageTest extends BaseTest {
+
+    @Test
+    public void isMyInfoPageLord() throws InterruptedException {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("Admin","admin123");
+        MyInfo myInfo = new MyInfo(driver);
+        myInfo.goMyInfo();
+        Assert.assertTrue(myInfo.isLordPage(),"BUG : My Info  Page Lord Attempt Fail ");
+    }
+}
